84b0c880a9b3951664871fe2e748dd3a682abb82
udaybhagwatgit <49365944+udaybhagwatgit@users.noreply.github.com>
2020-07-02 19:17:55 +0530

    Update AmpServiceProjectApplication.java

--- src/main/java/com/ampservices/main/AmpServiceProjectApplication.java
+++ src/main/java/com/ampservices/main/AmpServiceProjectApplication.java
@@ -15,5 +15,41 @@ public class AmpServiceProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AmpServiceProjectApplication.class, args);
 	}
+	
+	package com.ampservices.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@EnableMongoRepositories("com.ampservices.mongorepositories")
+@ComponentScan("com.ampservices")
+@SpringBootApplication
+@EnableAsync
+public class AmpServiceProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmpServiceProjectApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedHeaders("AMP-Access-Control-Allow-Source-Origin")
+						.exposedHeaders("AMP-Access-Control-Allow-Source-Origin")
+						.allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE").allowCredentials(true).maxAge(4800);
+			}
+		};
+	}
+}
+
 
 }
