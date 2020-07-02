4c91a424befd087e56a3089ac48ed69cb9ecb9e7
udaybhagwatgit <49365944+udaybhagwatgit@users.noreply.github.com>
2020-07-02 19:18:49 +0530

    Update AmpServiceProjectApplication.java

--- src/main/java/com/ampservices/main/AmpServiceProjectApplication.java
+++ src/main/java/com/ampservices/main/AmpServiceProjectApplication.java
@@ -2,24 +2,6 @@ package com.ampservices.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.scheduling.annotation.EnableAsync;
-
-@EnableMongoRepositories("com.ampservices.mongorepositories")
-@ComponentScan("com.ampservices")
-@SpringBootApplication
-@EnableAsync
-public class AmpServiceProjectApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(AmpServiceProjectApplication.class, args);
-	}
-	
-	package com.ampservices.main;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
@@ -49,7 +31,4 @@ public class AmpServiceProjectApplication {
 			}
 		};
 	}
-}
-
-
 }
