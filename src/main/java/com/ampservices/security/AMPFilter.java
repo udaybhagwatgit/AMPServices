94464fb95edd5ec3696c1a11542cb245af4308ed
udaybhagwatgit <49365944+udaybhagwatgit@users.noreply.github.com>
2020-07-06 21:59:32 +0530

    Create AMPFilter.java

---
+++ src/main/java/com/ampservices/security/AMPFilter.java
@@ -0,0 +1,42 @@
+package com.ampservices.security;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class AMPFilter implements Filter{
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		 HttpServletResponse httpServletResponse=(HttpServletResponse)response;
+	        httpServletResponse.setHeader("Access-Control-Allow-Origin","*");
+	        httpServletResponse.setHeader("AMP-Access-Control-Allow-Source-Origin", "*");
+	        httpServletResponse.setHeader("Access-Control-Allow-Credentials", "true");
+	        httpServletResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+	        httpServletResponse.setHeader("Access-Control-Allow-Headers", "origin, content-type, accept, x-requested-with, AMP-Access-Control-Allow-Source-Origin");
+	        httpServletResponse.setHeader("Access-Control-Expose-Headers", "AMP-Access-Control-Allow-Source-Origin");
+	        chain.doFilter(request, response);		
+	}
+
+	@Override
+	public void destroy() {
+		
+	}
+
+}
