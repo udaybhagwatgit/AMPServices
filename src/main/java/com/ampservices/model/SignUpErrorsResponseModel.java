40050d2a7d9ef7b4e14d8b5358ac9363dd5f6b68
udaybhagwatgit <49365944+udaybhagwatgit@users.noreply.github.com>
2020-07-09 19:11:51 +0530

    Add files via upload

---
+++ src/main/java/com/ampservices/model/SignUpErrorsResponseModel.java
@@ -0,0 +1,11 @@
+package com.ampservices.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SignUpErrorsResponseModel {
+	private String uri;
+	private String message;
+}
